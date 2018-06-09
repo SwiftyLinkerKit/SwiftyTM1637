4cb33737f3c1557cc77cad9cdfe389b7c8fe348d
Helge Heß <devteam@zeezide.de>
2018-06-09 16:58:04 +0200

    Add some letters
    ... for display

--- Sources/SwiftyTM1637/SevenSegmentRepresentable.swift
+++ Sources/SwiftyTM1637/SevenSegmentRepresentable.swift
@@ -26,9 +26,43 @@ extension Int : SevenSegmentRepresentable {
   
 }
 
+public extension SevenSegment {
+  
+  public static let dash : SevenSegment = [ .middleDash ] // -
+
+  public static let letters : [ Character : SevenSegment ] = [
+    "O": SevenSegment(digit: 0),
+    "A": SevenSegment(hexDigit: 0xA),
+    "B": SevenSegment(hexDigit: 0xB),
+    "C": SevenSegment(hexDigit: 0xC),
+    "D": SevenSegment(hexDigit: 0xD),
+    "E": SevenSegment(hexDigit: 0xE),
+    "F": SevenSegment(hexDigit: 0xF),
+    "S": [ .upperLeft,  .middleDash, .upperDash,  .lowerRight, .lowerDash  ],
+    "X": [ .upperRight, .middleDash, .upperLeft,  .lowerLeft,  .lowerRight ],
+    "Y": [ .upperRight, .middleDash, .upperLeft,  .lowerRight ],
+    "Z": [ .upperDash,  .upperRight, .middleDash, .lowerLeft,  .lowerDash  ],
+
+    "a": SevenSegment(hexDigit: 0xA),
+    "b": [ .upperLeft,  .middleDash, .lowerLeft,  .lowerRight, .lowerDash ],
+    "c": [ .middleDash, .lowerLeft,  .lowerDash ],
+    "d": [ .middleDash, .upperRight, .lowerLeft,  .lowerRight, .lowerDash ],
+    "h": [ .upperRight, .middleDash, .lowerLeft,  .lowerRight ],
+    "n": [ .lowerLeft,  .middleDash, .lowerRight ],
+    "o": [ .middleDash, .lowerLeft,  .lowerRight, .lowerDash  ],
+  ]
+}
+
 extension Character : SevenSegmentRepresentable {
   
   public var sevenSegmentValue : SevenSegment? {
+    if let s7 = SevenSegment.letters[self] { return s7 }
+    if self == " " { return .off }
+    
+    // use uppercase version if available
+    if let c  = String(self).uppercased().first,
+       let s7 = SevenSegment.letters[c] { return s7 }
+    
     // FIXME: add more!
     let utf8 = String(self).utf8
     guard utf8.count == 1, let c0 = utf8.first else { return nil }
